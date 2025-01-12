d2f473bef93c99f4022caeb33e2e0e3a51b24a88
Alessandro Gomes <alllessandrogomes@gmail.com>
2025-01-12 17:47:59 -0300

    :pencil: altera o ano do footer.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="bg-[color4] w-full flex flex-col items-center py-4 border-t-[0.5px] border-slate-800 text-center">
             <div className="flex flex-col items-center gap-4 px-6">
-                <h4>Copyright &copy; 2024 ValeBytes - Todos os direitos reservados</h4>
+                <h4>Copyright &copy; 2025 ValeBytes - Todos os direitos reservados</h4>
             </div>
         </footer>
     )
