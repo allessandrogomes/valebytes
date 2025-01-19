9e7624e013daa76e5f75df517290df0fef594fc5
Alessandro Gomes <alllessandrogomes@gmail.com>
2025-01-19 14:56:16 -0300

    :sparkles:feat: atualiza estados de envio do formulário.

--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -1,10 +1,13 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
 
 export default function ContactSection() {
 
     const [formData, setFormData] = useState({ name: "", phone: "", email: "", message: "" })
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [isSubmittedSucessfully, setIsSubmittedSucessfully] = useState<any>(null)
 
     const handleChange = (e: any) => {
         const { name, value } = e.target
@@ -13,21 +16,46 @@ export default function ContactSection() {
 
     const handleSubmit = async (e: any) => {
         e.preventDefault()
+        setIsSubmitting(true)
 
-        const response = await fetch("https://valebytes-api.vercel.app/api/send-email", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(formData)
-        })
+        try {
+            const response = await fetch("https://valebytes-api.vercel.app/api/send-email", {
+                method: "POST",
+                headers: { "Content-Type:": "application/json" },
+                body: JSON.stringify(formData)
+            })
+            
+            if (!response.ok) {
+                throw new Error("Erro ao enviar o formulário. Tente novamente.")
+            }
 
-        if (response.ok) {
-            const result = await response.json()
-            alert("Formulário enviado com sucesso. Entraremos em contato!")
-        } else {
-            const error = await response.json()
-            alert(`Erro ao enviar o formulário: ${error.message}`)
+            const data = await response.json()
+
+            if (!data.success) {
+                throw new Error("Erro ao enviar o formulário. Tente novamente.")
+            }
+
+            setIsSubmittedSucessfully(true)
+            setIsSubmitting(false)
+            setFormData({
+                name: "",
+                phone: "",
+                email: "",
+                message: ""
+            })
+        } catch (error) {
+            setIsSubmitting(false)
+            setIsSubmittedSucessfully(false)
         }
     }
+
+    useEffect(() => {
+        if (isSubmittedSucessfully === true) {
+            setTimeout(() => {
+                setIsSubmittedSucessfully(null)
+            }, 5000)
+        }
+    }, [isSubmittedSucessfully])
 
     return (
         <section className="flex flex-wrap items-center justify-center gap-[50px] w-full max-w-full bg-contact bg-cover bg-no-repeat pb-[100px] md:pb-[200px] px-6 xl:px-0" id="contato">
@@ -35,28 +63,43 @@ export default function ContactSection() {
                 <h2 className="capitalize font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-6xl mb-4">Vamos discutir<br /> seu projeto</h2>
                 <h3 className="text-base sm:text-lg md:text-xl">Preencha o formulário com suas informações e uma breve descrição do seu projeto. Entraremos em contato para discutir como podemos ajudar.</h3>
             </div>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4 bg-color5 w-[500px] text-color3 font-medium p-8 rounded-lg">
+            <form onSubmit={e => handleSubmit(e)} className="flex flex-col gap-4 bg-color5 w-[500px] text-color3 font-medium p-8 rounded-lg">
                 <div className="flex flex-col">
                     <label>Nome/Empresa</label>
-                    <input onChange={handleChange} name="name" className="border border-color3 bg-color5 outline-none text-black font-normal p-2 rounded-lg" type="text" required />
+                    <input onChange={handleChange} value={formData.name} name="name" className="border border-color3 bg-color5 outline-none text-black font-normal p-2 rounded-lg" type="text" required />
                 </div>
 
                 <div className="flex flex-col">
                     <label>Telefone</label>
-                    <input onChange={handleChange} name="phone" className="border border-color3 bg-color5 outline-none text-black font-normal p-2 rounded-lg" type="text" required />
+                    <input onChange={handleChange} value={formData.phone} name="phone" className="border border-color3 bg-color5 outline-none text-black font-normal p-2 rounded-lg" type="text" required />
                 </div>
 
                 <div className="flex flex-col">
                     <label>Email</label>
-                    <input onChange={handleChange} name="email" className="border border-color3 bg-color5 outline-none text-black font-normal p-2 rounded-lg" type="email" required />
+                    <input onChange={handleChange} value={formData.email} name="email" className="border border-color3 bg-color5 outline-none text-black font-normal p-2 rounded-lg" type="email" required />
                 </div>
 
                 <div className="flex flex-col">
                     <label>Conte-nos brevemente seu Projeto</label>
-                    <textarea onChange={handleChange} name="message" className="h-[100px] border border-color3 bg-color5 outline-none text-black font-normal p-2 rounded-lg resize-none" maxLength={500} required></textarea>
+                    <textarea onChange={handleChange} value={formData.message} name="message" className="h-[100px] border border-color3 bg-color5 outline-none text-black font-normal p-2 rounded-lg resize-none" maxLength={500} required></textarea>
                 </div>
 
-                <button type="submit" className="bg-color3 w-max self-center rounded-lg text-white p-2">Enviar</button>
+                <button disabled={isSubmitting} type="submit" className={`${isSubmitting ? "bg-[#cccccc]" : "bg-color3"} w-max self-center rounded-lg text-white p-2`}>
+                    {isSubmitting
+                        ?
+                        <p className="flex gap-2 items-center">Enviando <AiOutlineLoading3Quarters className="spinner" style={{ animation: 'spin 1s linear infinite' }} /></p>
+                        :
+                        "Enviar"
+                    }
+                </button>
+
+                {isSubmittedSucessfully === false && (
+                    <p className="self-center text-red-600">Não foi possível enviar o formulário. Tente novamente.</p>
+                )}
+
+                {isSubmittedSucessfully === true && (
+                    <p className="self-center">Enviado com sucesso, entraremos em contato!</p>
+                )}
             </form>
         </section>
     )
