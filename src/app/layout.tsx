967019c8681b67ee0b1f9449e22d648931a52f58
Alessandro Gomes <alllessandrogomes@gmail.com>
2025-01-22 10:55:13 -0300

    :mag: atualiza metadatas e adiciona scripts do google analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,23 @@
 import type { Metadata } from "next"
+import Script from "next/script"
 
 export const metadata: Metadata = {
-    title: 'Criação de Sites em Juazeiro e Petrolina | ValeBytes',
+    title: 'Criação de Sites - ValeBytes',
     description: 'A ValeBytes é uma empresa especializada em criação de sites em Juazeiro BA e Petrolina PE, oferecendo soluções personalizadas para sua presença online com design e tecnologia de ponta.',
     keywords: ['Criação de sites Juazeiro BA', 'Criação de sites Petrolina PE', 'Desenvolvimento de sites Vale do São Francisco', 'Empresa criação de sites Juazeiro', 'Criar site Petrolina', 'Agência web Juazeiro e Petrolina', 'Sites personalizados Juazeiro BA', 'Soluções web em Petrolina', 'Landing pages Juazeiro', 'Desenvolvimento web Vale do São Francisco', 'sites para pequenos negócios em Petrolina', 'agência de sites em Petrolina e Juazeiro', 'site profissional para empresas em Juazeiro', 'desenvolvimento de sites para empresas em Petrolina-PE', 'criação de sites em Juazeiro-BA'],
     robots: 'index, follow',
     openGraph: {
+        locale: "pt_BR",
+        siteName: "ValeBytes",
         type: 'website',
-        title: 'Criação de Sites em Juazeiro e Petrolina | ValeBytes',
+        title: 'Criação de Sites em - ValeBytes',
         description: 'A ValeBytes é uma empresa especializada em criação de sites em Juazeiro BA e Petrolina PE, oferecendo soluções personalizadas para sua presença online com design e tecnologia de ponta.',
         url: 'https://www.valebytes.com.br',
         images: 'https://valebytes.com.br/images/valebytes-logo-transparent.png'
     },
     twitter: {
         card: 'summary_large_image',
-        title: 'Criação de Sites em Juazeiro e Petrolina | ValeBytes',
+        title: 'Criação de Sites em - ValeBytes',
         description: 'A ValeBytes é uma empresa especializada em criação de sites em Juazeiro BA e Petrolina PE, oferecendo soluções personalizadas para sua presença online com design e tecnologia de ponta.',
         images: 'https://valebytes.com.br/images/valebytes-logo-transparent.png'
     },
@@ -30,6 +33,21 @@ export default function RootLayout({
 }) {
     return (
         <html lang="pt-BR">
+            <head>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T1T72L0SFT" />
+                <Script
+                    id="google-analytics"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-T1T72L0SFT');
+                        `,
+                    }}
+                />
+
+            </head>
             <body>
                 {/* Layout UI */}
                 <main>{children}</main>
